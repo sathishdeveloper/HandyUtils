0688b6a8d19c9ec413a223f0fae3e3767f808893
sathishdeveloper <1582428+sathishdeveloper@users.noreply.github.com>
2019-01-24 14:22:35 +0530

    Update StringToInteger.java

--- StringToInteger.java
+++ StringToInteger.java
@@ -1,12 +1,11 @@
 public class StringToInteger {
-  static int toInteger(final String str) {
+	static int toInteger(final String str) {
 		int num = 0, factor = 1;
-
 		for(int i=str.length()-1; i>=0; --i) {
 			num += (str.charAt(i) - '0') * factor;
 			factor *=10;
 		}
 		System.out.println(num);
-    return num;
-  }
+		return num;
+	}
 }
